e52fefddd8501fef497b7549d75a1ebe354d8424
i7allia <76435852+i7allia@users.noreply.github.com>
2021-05-21 14:23:07 +0300

    Переделал
    Убрал лишние поля в странице add

--- Mediaced/Model/Advertisement.cs
+++ Mediaced/Model/Advertisement.cs
@@ -8,48 +8,35 @@ namespace Mediaced.Model
     public class Advertisement
     {
         public int id { get; set; }
-        public String firstName { get; set; }
-        public String lastName { get; set; }
-        public String patronimic { get; set; }
+        public String name { get; set; }
         public String phoneNumber { get; set; }
         public String email { get; set; }
-        public int age { get; set; }
-        public String advText { get; set; }
-        public float priceUSD { get; set; }
+        public float salaryusd { get; set; }
         public float courseOfUSD { get; set; }
-        public float priceBYN { get; set; }
+        public float salarybyn { get; set; }
 
 
-        public Advertisement(int id, String firstName, String lastName, String patronimic,
-            String phoneNumber, String email, int age, String advText, float priceUSD,
-            float courseOfUSD, float priceBYN)
+        public Advertisement(int id, String name, String phoneNumber, String email, float salaryusd,
+            float courseOfUSD, float salarybyn)
         {
             this.id = id;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.patronimic = patronimic;
+            this.name = name;
             this.phoneNumber = phoneNumber;
             this.email = email;
-            this.age = age;
-            this.advText = advText;
-            this.priceUSD = priceUSD;
+            this.salaryusd = salaryusd;
             this.courseOfUSD = courseOfUSD;
-            this.priceBYN = priceBYN;
+            this.salarybyn = salarybyn;
         }
 
         public Advertisement()
         {
             this.id = 0;
-            this.firstName = "";
-            this.lastName = "";
-            this.patronimic = "";
+            this.name = "";
             this.phoneNumber = "";
             this.email = "";
-            this.age = 0;
-            this.advText = "";
-            this.priceUSD = 0.0f;
+            this.salaryusd = 0.0f;
             this.courseOfUSD = 0.0f;
-            this.priceBYN = 0.0f;
+            this.salarybyn = 0.0f;
         }
 
     }
